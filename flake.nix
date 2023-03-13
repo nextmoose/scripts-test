9bd973b52d1f05cf64e142d8f488bde3f56ca6b3
Emory Merryman <emory.merryman@gmail.com>
2023-03-13 11:50:01 -0400

    reinit

---
+++ flake.nix
@@ -0,0 +1,16 @@
+  {
+    inputs =
+      {
+        flake-utils.url = "github:numtide/flake-utils?rev=5aed5285a952e0b949eb3ba02c12fa4fcfef535f" ;
+        nixpkgs.url = "github:nixos/nixpkgs?rev=57eac89459226f3ec743ffa6bbbc1042f5836843" ;
+      } ;
+    outputs =
+      { flake-utils , self , nixpkgs } :
+        flake-utils.lib.eachDefaultSystem
+          (
+            system :
+              {
+                lib = tester : tester ( implementation : true ) true true ;
+              }
+          ) ;
+  }
