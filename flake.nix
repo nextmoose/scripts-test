b69c14c088693a27c2dfa31158e8fbaa3cb7a4ed
Emory Merryman <emory.merryman@gmail.com>
2023-03-13 12:20:15 -0400

    typo

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
               {
                 lib =
                   {
-                    foobar = tester : tester ( implementation : implementation "d1b60e0a-dfee-47a7-b4f5-1d8d88f1cd05" ) true ( builtins.readFile ./scripts/foobar.sh ) ) ;
+                    foobar = tester : tester ( implementation : implementation "d1b60e0a-dfee-47a7-b4f5-1d8d88f1cd05" ) true ( builtins.readFile ./scripts/foobar.sh ) ;
                   } ;
               }
           ) ;
